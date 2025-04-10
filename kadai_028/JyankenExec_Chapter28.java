b04a60e7fcb69796b0c05f4901d210f4a1dff6a6
ayase1998 <caishijigang@gmail.com>
2025-04-10 15:09:51 +0900

    kadai_028追加

---
+++ kadai_028/JyankenExec_Chapter28.java
@@ -0,0 +1,26 @@
+package kadai_028;
+
+public class JyankenExec_Chapter28 {
+
+	public static void main(String[] args) {
+		
+		Jyanken_Chapter28 jyanken = new Jyanken_Chapter28();
+		jyanken.playGame();
+		
+	}
+		
+		/*System.out.println("もう一度対戦しますか？");
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.next();
+		
+		if(input.equals("y")) {
+			jyanken.playGame();
+		} else if(input.equals("n")) {
+			System.out.println("ゲームを終了します。");
+		}else {
+		}
+			System.out.println("不正な文字が入力されました。");
+		} */
+		
+}
